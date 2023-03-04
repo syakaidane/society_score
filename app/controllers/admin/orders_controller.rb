1fcf481efbe616b89d15aa2c229e06dbfa945780
syakaidane <121926213+syakaidane@users.noreply.github.com>
2023-03-04 01:06:48 +0000

    admin order function

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,10 @@
 class Admin::OrdersController < ApplicationController
+  
+  def index
+    @orders = Order.page(params[:page]).per(10).order(created_at: "DESC")
+  end
+  
   def show
+    @order = Order.find(params[:id])
   end
 end
